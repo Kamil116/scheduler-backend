5489185cf39800860130fec9a79ad2fe04e4eaad
Kamil116 <kamil.mirgasimoff@yandex.ru>
2023-07-16 12:10:56 +0300

    testing deployment

--- parsedDataToDatabase.py
+++ parsedDataToDatabase.py
@@ -9,14 +9,14 @@ json_file = 'output.json'
 with open(json_file) as json_data:
     data = json.load(json_data)
 
-if len(coursesDatabase.get_courses()) == 0:
-    for course in data:
-        try:
-            coursesDatabase.add_course(course['title'], course['start'], course['end'], course['extendedProps']['room'],
-                                       course['extendedProps']['course'], course['extendedProps']['group'],
-                                       course['extendedProps']['instructorName'])
-        except KeyError:
-            # In case when we do not have info about group. It is lectures and tutorials.
-            coursesDatabase.add_course(course['title'], course['start'], course['end'], course['extendedProps']['room'],
-                                       course['extendedProps']['course'], "None",
-                                       course['extendedProps']['instructorName'])
+# if len(coursesDatabase.get_courses()) == 0:
+#     for course in data:
+#         try:
+#             coursesDatabase.add_course(course['title'], course['start'], course['end'], course['extendedProps']['room'],
+#                                        course['extendedProps']['course'], course['extendedProps']['group'],
+#                                        course['extendedProps']['instructorName'])
+#         except KeyError:
+#             # In case when we do not have info about group. It is lectures and tutorials.
+#             coursesDatabase.add_course(course['title'], course['start'], course['end'], course['extendedProps']['room'],
+#                                        course['extendedProps']['course'], "None",
+#                                        course['extendedProps']['instructorName'])
